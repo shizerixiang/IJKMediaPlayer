deec7ecd17f65b174f9a61e1d6fa586938ad65ed
王标 <shizerixiang@hotmail.com>
2018-03-27 19:59:53 +0800

    22

---
+++ app/src/main/java/com/beviswang/ijkmediaplayer/utils/DrawHelper.kt
@@ -0,0 +1,8 @@
+package com.beviswang.ijkmediaplayer.utils
+
+/**
+ * 绘制工具类
+ * Created by 王标 on 2018/3/27.
+ */
+class DrawHelper {
+}
---
+++ app/src/main/java/com/beviswang/ijkmediaplayer/widget/MusicTabLayout.kt
@@ -0,0 +1,19 @@
+package com.beviswang.ijkmediaplayer.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.support.v7.widget.AppCompatTextView
+import android.util.AttributeSet
+
+/**
+ * 仿虾米音乐 TabLayout
+ * Created by 王标 on 2018/3/27.
+ */
+class MusicTabLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null,
+                                               def: Int = 0)
+    : AppCompatTextView(context, attrs, def) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+}
